a842181a27dc6f52046b8369644f135d322e8045
marcomera <marcomera@icloud.com>
2017-07-13 16:00:27 -0500

    Trying to add functionality for multiple items on the todo list

--- functional_test.py
+++ functional_test.py
@@ -10,7 +10,12 @@ class NewVisitorTest(unittest.TestCase):
 
 	def tearDown(self):
 		self.browser.quit()
-
+	
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+	
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
@@ -27,12 +32,8 @@ class NewVisitorTest(unittest.TestCase):
 
 		input_box.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
-			,"New to-do item did not appear in table")
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		self.fail('Finish the test!')
 
 	if __name__ == '__main__':
