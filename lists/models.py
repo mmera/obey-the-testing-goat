afcfcf411c8389a19e74a57c9bada9eb8fe21726
marcomera <marcomera@icloud.com>
2017-07-13 16:16:05 -0500

    Model for list Items and associated migration

--- lists/models.py
+++ lists/models.py
@@ -4,3 +4,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+	text = models.TextField(default='')
--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,27 @@ from django.test import Client
 from django.http import HttpRequest
 
 from lists.views import home_page
+from lists.models import Item 
+
+class itemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
 
 class HomePageTest(TestCase):
 	
