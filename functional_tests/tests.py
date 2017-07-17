c5868b9be7786d427130131e7f44f0b4c702394b
marcomera <marcomera@icloud.com>
2017-07-17 16:04:27 -0500

    Added FT making sure each user gets their own URL

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,12 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException 
 import time
 
 class NewVisitorTest(LiveServerTestCase):
+
+	
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -11,10 +14,19 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 	
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		MAX_WAIT = 10
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return 
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 	
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#Edith has heard about a cool new online to-do app.
@@ -36,22 +48,67 @@ class NewVisitorTest(LiveServerTestCase):
 		#She types "Buy peacock feathers" into a text box
 		input_box.send_keys('Buy peacock feathers')
 		input_box.send_keys(Keys.ENTER)
-		time.sleep(1)
 
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 		#There is still a text box inviting her to add another item. 
 		#She enters "Use peacock feathers to make a fly" 
 		input_box = self.browser.find_element_by_id('id_new_item')
 		input_box.send_keys('Use peacock feathers to make a fly')
 		input_box.send_keys(Keys.ENTER)
-		time.sleep(1)
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
-		self.check_for_row_in_list_table(u'2: Use peacock feathers to make a fly')
-		self.check_for_row_in_list_table(u'1: Buy peacock feathers')
+		self.wait_for_row_in_list_table(u'2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table(u'1: Buy peacock feathers')
 		
 
 		self.fail('Finish the test!')
+
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		self.browser.get(self.live_server_url)
+		input_box = self.browser.find_element_by_id('id_new_item')
+		input_box.send_keys('Buy peacock feathers')
+		input_box.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
+
+		#New user Francis
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		#Make sure Edith's list isn't viewable 
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		#Francis starts his own list
+		input_box = self.browser.find_elements_by_id('id_new_item')
+		input_box.send_keys('Buy milk')
+		input_box.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+
+		#Francis get his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url,edith_list_url)
+
+		#Again, there is no trace of Edith's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+
+
+
+
+
+
+
+
+
+
--- lists/tests.py
+++ lists/tests.py
@@ -32,7 +32,7 @@ class HomePageTest(TestCase):
 	
 	def test_uses_home_template(self):
 		response = self.client.get('/')
-		self.assertTemplateUsed(response,'home.html')
+		self.assertTemplateUsed(response, 'home.html')
 
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
