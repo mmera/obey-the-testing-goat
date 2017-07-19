05d43e0269cbed878c8faae71c275fa2aec69f4e
marcomera <marcomera@icloud.com>
2017-07-19 15:21:28 -0500

    Use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException 
@@ -6,7 +6,7 @@ import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -43,7 +43,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.wait_for_row_in_list_table('1: testing')
 		input_box = self.browser.find_element_by_id('id_new_item')
 		self.assertAlmostEqual(
-		input_box.location['x'] + inputbox.size['width'] / 2,
+		input_box.location['x'] + input_box.size['width'] / 2,
 		512,
 		delta=10
 	)
