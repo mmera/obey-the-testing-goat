ccdb39e9f0704700169d5a1745a10f972a39a5dd
marcomera <marcomera@icloud.com>
2017-08-02 16:02:11 -0500

    new form for list items

---
+++ lists/tests/test_models.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from lists.models import Item, List
+
+
+class ListAndItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.list = list_
+		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
+		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
+
+	def test_cannot_save_empty_list_items(self):
+		list_ = List.objects.create()
+		item = Item(list=list_, text='')
+		with self.assertRaises(ValidationError):
+			item.save()
+			item.full_clean()
+
+	def test_get_absolute_url(self):
+		list_ = List.objects.create()
+		self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+		
